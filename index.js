232dff91a092d17a7e4e9210f3276054e5d411a8
heshuo <2393097587@qq.com>
2022-11-08 00:38:08 +0800

    防抖和节流

--- index.js
+++ index.js
@@ -3,13 +3,28 @@
  * 防抖
  */
 const but = document.querySelector('.button')
+but.addEventListener('click', debounce(debounceEvent, 200))
+const but2 = document.querySelector('.button2')
+but2.addEventListener('click', throttle(debounceEvent2, 200))
+const but3 = document.querySelector('.button3')
+but3.addEventListener('click', deThrottle(debounceEvent3, 500))
+
+function debounceEvent() {
+    console.log('防抖了');
+}
+function debounceEvent2() {
+    console.log('节流了');
+}
+function debounceEvent3() {
+    console.log('防抖和节流了');
+}
 
 // 防抖
 function debounce(fn, delay) {
     let time = null
     return () => {
         clearTimeout(time)
-        setTimeout(() => {
+        time = setTimeout(() => {
             fn.call(this.arguments)
         }, delay);
     }
@@ -18,30 +33,39 @@ function debounce(fn, delay) {
 // 节流
 function throttle(fn, delay) {
     let time = true
-    if (time) {
-        time= false
+    return () => {
+        if (!time) {
+            return
+        }
+        time = false
         setTimeout(() => {
             time = true
-            fn.call(this.arguments)
+            fn.apply(this, arguments)
         }, delay);
     }
 }
 
-/* 
-const arr = [1, [[2, 3], 4],5]
-const flatten = (arr, deep = 1) => {
-    if (deep <= 0) return arr;
-    return arr.reduce((res, curr) => res.concat(Array.isArray(curr) ? flatten(curr, deep - 1) : curr), [])
+
+function deThrottle(fn, delay) {
+    let last = 0, timer = null;
+    return function (...args) {
+        let now = new Date();
+        if (now - last < delay) {
+            clearTimeout(timer);
+            setTimeout(function () {
+                last = now;
+                fn.apply(this, args);
+            }, delay);
+        } else {
+            // 这个时候表示时间到了，必须给响应
+            last = now;
+            fn.apply(this, args);
+        }
+    }
 }
-// function flatten (arr,deep=1) {
-// return   arr.reduce((acc,val) => acc.concat(Array.isArray(val)? flatten(val,deep-1):val),[])
-// }
-console.log(arr, Infinity);
-// 输出:[ 1, 2, 3, 4, 5, 6 ]
- 
-*/
 
 
+//  手写一个flatten 数组扁平
 /* function flatten(arr) {
     while (arr.some(item => Array.isArray(item))) {
         arr = [].concat(...arr)
